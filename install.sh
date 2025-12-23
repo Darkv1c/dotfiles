09b5720ce3b99eaeb463f161de40fac49e732037
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 01:04:53 -0500

    Add oh-my-posh installation and font configuration
    Added installation steps for oh-my-posh and font setup.

--- install.sh
+++ install.sh
@@ -9,3 +9,9 @@ wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_6
 chmod +x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract 
 mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
+
+# Set up oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+oh-my-posh font install Hack
+sudo cp ./Hack*.ttf /usr/share/fonts/
+echo "eval \"$(oh-my-posh init bash --config /usr/share/oh-my-posh/themes/jblab_2021.omp.json)\"" >> ~/.bashrc
