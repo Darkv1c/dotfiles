ae55d2104b949325bada78e605eaf03c56931f25
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 00:08:33 -0500

    Update install script to install Neovim directly
    Removed apt upgrade commands and added installation steps for Neovim.

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@
 export TERM=xterm-256color
 echo 'export TERM=xterm-256color' >> ~/.bashrc
 
-# apt upgrade
-sudo apt-get update
-sudo apt-get upgrade
-
-yes | sudo apt install neovim
+# Install nvim
+wget http://ftp.osuosl.org/pub/ubuntu/pool/universe/n/neovim/neovim_0.11.5-2_amd64.deb
+sudo dpkg -i neovim_0.11.5-2_amd64.deb
+yes | sudo apt --fix-broken install
