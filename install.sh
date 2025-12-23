a265766260c4f30f81420d0b6d1d5cba3fb44b88
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 01:35:08 -0500

    Comment out font installation for oh-my-posh
    Comment out oh-my-posh font installation commands.

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
 
 # Set up oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
-oh-my-posh font install Hack
-sudo cp ./Hack*.ttf /usr/share/fonts/
+#oh-my-posh font install Hack
+#sudo cp ./Hack*.ttf /usr/share/fonts/
 mkdir -p ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
