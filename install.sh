685252974a92e25cf58d7b8136d559fd79bea9af
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 00:32:27 -0500

    Change Neovim installation to AppImage format
    Updated Neovim installation method to use AppImage.

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ export TERM=xterm-256color
 echo 'export TERM=xterm-256color' >> ~/.bashrc
 
 # Install nvim
-wget http://ftp.osuosl.org/pub/ubuntu/pool/universe/n/neovim/neovim_0.11.5-2_amd64.deb
-sudo dpkg -i neovim_0.11.5-2_amd64.deb
-yes | sudo apt --fix-broken install
+wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage --appimage-extract 
+mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
