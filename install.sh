e2f0c608c6c2f7b71f0c07c37da9eb8c6edef2e1
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 00:38:55 -0500

    Fix install script to make nvim executable

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ echo 'export TERM=xterm-256color' >> ~/.bashrc
 
 # Install nvim
 wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.appimage
+chmod +x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract 
 mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
